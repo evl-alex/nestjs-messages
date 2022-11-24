2506ee77b0d85a8df28f2bb013cbd7675eb6989d
Oleksandr Bibik <obibik@emergemarket.com>
2022-11-24 16:56:37 +0200

    Added dependency injection

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
-
 bootstrap();
